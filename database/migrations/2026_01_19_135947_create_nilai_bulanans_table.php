132ca700ac9d001078268248e4c53d2f756a4dc8
robbytrujillo <robbyikusuma88@gmail.com>
2026-01-25 09:24:09 +0700

    raportku SMA IHBS

--- database/migrations/2026_01_19_135947_create_nilai_bulanans_table.php
+++ database/migrations/2026_01_19_135947_create_nilai_bulanans_table.php
@@ -15,30 +15,15 @@ return new class extends Migration
     {
         Schema::create('nilai_bulanans', function (Blueprint $table) {
             $table->id();
-
-            $table->foreignId('siswa_id')
-          ->constrained('siswas')
-          ->cascadeOnDelete();
-
-            $table->foreignId('pembelajaran_id')
-                ->constrained('pembelajarans')
-                ->cascadeOnDelete();
-
-            $table->tinyInteger('bulan');      // 1–12
-            $table->tinyInteger('semester');   // 1 / 2
+            $table->foreignId('siswa_id')->constrained('siswas')->onDelete('cascade');
+            $table->foreignId('pembelajaran_id')->constrained('pembelajarans')->onDelete('cascade');
+            $table->tinyInteger('bulan');
+            $table->tinyInteger('semester');
             $table->year('tahun');
-
-            $table->integer('nilai')->nullable();
-            $table->text('capaian_tp_optimal')->nullable();
-            $table->text('capaian_tp_kurang')->nullable();
-            $table->text('deskripsi')->nullable();
-
+            $table->bigInteger('nilai');
+            $table->text('deskripsi_capaian_tinggi')->nullable();
+            $table->text('deskripsi_capaian_rendah')->nullable();
             $table->timestamps();
-
-            $table->unique(
-                ['siswa_id', 'pembelajaran_id', 'bulan', 'semester', 'tahun'],
-                'nilai_bulanan_unique'
-            );
         });
     }
 
