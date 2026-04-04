8f25cf4b67ca4c46306fe63774a1f12feb66280f
robbytrujillo <robbyikusuma88@gmail.com>
2026-04-05 05:33:16 +0700

    Introduction raportku

---
+++ database/migrations/2026_04_04_062409_add_ttd_to_gurus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('gurus', function (Blueprint $table) {
+            //
+            $table->string('ttd')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('gurus', function (Blueprint $table) {
+            //
+            $table->dropColumn('ttd');
+        });
+    }
+};
