5b8ec0b849455a55a48883604e99f29d8abe8a69
robbytrujillo <robbyikusuma88@gmail.com>
2026-04-30 13:23:34 +0700

    Introduction raportku

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,8 @@
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <strong>Copyright &copy; {{ date('Y') }} <a href="#">RAPORTKU SMA IHBS</a></strong>
+    <strong>Copyright &copy; {{ date('Y') }} <a href="#">RAPORTKU SMA</a></strong>
 
     <div class="float-right d-none d-sm-inline-block">
-      by <a href="https://robbyilham.com" target="_blank">robbyilham</a>
+      <a href="https://robbyilham.com" target="_blank">by</a> IT Devolepment IHBS
     </div>
   </footer>
